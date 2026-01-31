0d1622fc75b504696aa923054cfec1a74fa2a6be
harshhariyani404 <harsh.hariyani404@gmail.com>
2026-01-31 16:27:46 +0530

    add search Dimonds

--- .history/src/sections/Nevbar_20250710143135.jsx
+++ .history/src/sections/Nevbar_20250710143135.jsx
@@ -14,7 +14,6 @@ const Navbar = () => {
         ☰
       </div>
       <ul className={`${styles.navlinks} ${menuOpen ? styles.open : ""}`}>
-        <li><a href="https://ds-diamonds.netlify.app/">Search Diamonds</a></li>
         <li><a href="/">Home</a></li>
         <li><a href="/products">Products</a></li>
         <li><a href="/about">About</a></li>
